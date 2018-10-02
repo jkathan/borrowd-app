6c00b9311e26ead1e237250c0934e6dadb902443
Joey Kathan <kathan.joey@gmail.com>
2018-10-02 12:43:14 -0400

    editing index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import LoanItemBoard from './components/loan-item-list'
 
 ReactDOM.render(<LoanItemList />, document.getElementById('root'));
 registerServiceWorker();
