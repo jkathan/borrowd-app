5253d510105bc151f6a86449a47d45992fb78e75
Joey Kathan <kathan.joey@gmail.com>
2018-10-02 12:41:03 -0400

    editing index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoanItemList />, document.getElementById('root'));
 registerServiceWorker();
