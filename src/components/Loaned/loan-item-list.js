2b293749be839a36aa073db5631ec120a44a9761
Joey Kathan <kathan.joey@gmail.com>
2018-10-02 13:06:19 -0400

    editing index.js

--- src/components/Loaned/loan-item-list.js
+++ src/components/Loaned/loan-item-list.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import {connect} from 'react-redux';
 
 import LoanCard from './loan-card'
-import AddLoanForm from './add-form';
+import AddLoanForm from './loan-card-form';
 
 //import {addCard} from '../actions';
 
