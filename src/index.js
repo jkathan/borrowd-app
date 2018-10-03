2d6de5c6dbe3fa897121fc6a8cdcf243f2873660
Joey Kathan <kathan.joey@gmail.com>
2018-10-03 01:37:27 -0400

    static pages to work

--- src/index.js
+++ src/index.js
@@ -13,15 +13,16 @@ const ITEMLOAN = [
   {type: 'tool', item: 'Wrench'},
   {type: 'tool', item: 'Wrench'},
 ];
-
+/*
 const ITEMBORROW = [
   {type: 'tool', item: 'Wrench', owner: "Joey"},
+  {type: 'tool', item: 'Tornado', owner: "Joey"},
   {type: 'tool', item: 'Wrench', owner: "Joey"},
-  {type: 'tool', item: 'Wrench', owner: "Joey"},
-  {type: 'tool', item: 'Wrench', owner: "Joey"},
+  {type: 'tool', item: 'Turtle', owner: "Joey"},
   {type: 'tool', item: 'Wrench', owner: "Joey"},
   {type: 'tool', item: 'Wrench', owner: "Joey"},
 ];
+*/
 
 ReactDOM.render(<ItemPage itemLoan={ITEMLOAN} itemBorrow={ITEMBORROW} />, document.getElementById('root'));
 registerServiceWorker();
